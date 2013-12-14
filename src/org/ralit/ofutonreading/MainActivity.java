85cfcafa2fb7a5c41f70ff4287eafb5ba9006a3c
ralit <ralit@ralitMacBookProMid2012.local>
2013-12-14 18:03:32 +0900

    splashView

--- src/org/ralit/ofutonreading/MainActivity.java
+++ src/org/ralit/ofutonreading/MainActivity.java
@@ -2,55 +2,36 @@ package org.ralit.ofutonreading;
 
 import java.io.File;
 
+import android.animation.AnimatorSet;
+import android.animation.ObjectAnimator;
 import android.app.Activity;
 import android.os.Bundle;
-import android.os.Environment;
 import android.view.Menu;
 import android.view.MenuItem;
-import android.view.View;
-import android.view.ViewGroup.LayoutParams;
+import android.widget.FrameLayout;
 
-public class MainActivity extends Activity implements OpenFileListener, FileOpenDialogListener, FileClickListener {
-	
-	private FileClickListener fileClickListener;
+public class MainActivity extends Activity implements FileClickListener {
 	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		Fun.log("onCreate()");
 		super.onCreate(savedInstanceState);
-//		Fun.log(String.valueOf(Fun.match("abcde", "bcd", true)));
-//		Fun.log(Fun.matchGroup("01234", "([0-9])([0-9])", true).toString());
-//		Fun.log(String.valueOf(Fun.matchGroup("chuhcewuihwe", "([0-9])", true)));
-//		ArrayList<ArrayList<String>> tmp = Fun.matchGroup("chuhcewuihwe", "([a-z])", true);
-//		for(int i = 0; i < tmp.size(); i++) {
-//			ArrayList<String> tmp2 = tmp.get(i);
-//			for(int j = 0; j < tmp2.size(); j++) {
-//				String str = tmp2.get(j);
-//				Fun.log(str);
-//			}
-//		}
-//		Fun.log(String.valueOf(Fun.matchGroup("/storage/sdcard0/imagemove", "(/storage/sdcard0/imagemove\\/.+?)(\\/|$)", false)));
-//		Fun.log(Fun.getExternalStoragePath());
-//		LinearLayout layout = new LinearLayout(this);
-//		setContentView(layout);
-		
-//		ListView mListView = new ListView(this);
-//		setContentView(mListView);
-//		String[] test = {"111", "fhwuei", "iowceow", "jifoewjf", "111", "fhwuei", "iowceow", "jifoewjf", "111", "fhwuei", "iowceow", "jifoewjf" };
-//		ArrayAdapter<String> adapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, test);
-//		mListView.setAdapter(adapter);
-//		BookView view = new BookView(this);
-//		setContentView(view);
-//
-//		LayoutParams params = view.getLayoutParams();
-//		params.width = LayoutParams.MATCH_PARENT;
-//		params.height = LayoutParams.MATCH_PARENT;
-//		view.setLayoutParams(params);
-
+		FrameLayout frameLayout = new FrameLayout(this);
+		setContentView(frameLayout);
 		FileListView fileListView = new FileListView(this, this);
-		setContentView(fileListView);
-//		CardListView cardListView = new CardListView(this);
-//		setContentView(cardListView);
+		frameLayout.addView(fileListView);
+		SplashView splashView = new SplashView(this);
+		frameLayout.addView(splashView);
+		
+		AnimatorSet set = new AnimatorSet();
+		ObjectAnimator animator = ObjectAnimator.ofFloat(splashView, "alpha", 1f);
+		ObjectAnimator animator2 = ObjectAnimator.ofFloat(splashView, "alpha", 0f);
+		animator.setDuration(2000);
+		animator2.setDuration(500);
+		set.playSequentially(animator, animator2);
+		set.start();
+//		Configuration config = getResources().getConfiguration();
+//		config.orientation = Configuration.ORIENTATION_LANDSCAPE;
 	}
 
 //	@Override
@@ -70,23 +51,8 @@ public class MainActivity extends Activity implements OpenFileListener, FileOpen
 	public boolean onOptionsItemSelected(MenuItem item) {
 		Fun.log("onOptionsItemSelected()");
 		switch (item.getItemId()) {
-		case R.id.ofuton_open_file: {
-//			OpenFile openFile = new OpenFile(this, this);
-//			openFile.openDir(Environment.getExternalStorageDirectory().getAbsolutePath() + "/imagemove");
-//			openFile.openDir(null, true);
-			FileOpenDialog fileOpenDialog = new FileOpenDialog(this, this, false);
-			fileOpenDialog.openDirectory(Environment.getExternalStorageDirectory().getAbsolutePath() + "/imagemove");
-			return true;
-		}
 		}
 		return false;
-	}
-
-	@Override
-	public void onFileSelected(File file) {
-		Fun.log("onFileSelected()");
-//		Fun.log(file.getParentFile().getAbsolutePath());
-		Fun.saveRoot(file.getParentFile().getAbsolutePath(), "lastDir.txt");
 	}
 
 	@Override
---
+++ src/org/ralit/ofutonreading/SplashView.java
@@ -0,0 +1,62 @@
+package org.ralit.ofutonreading;
+
+import android.content.Context;
+import android.graphics.Color;
+import android.graphics.Typeface;
+import android.view.Gravity;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+public class SplashView extends ViewGroup{
+	
+	public SplashView(Context context) {
+		super(context);
+		TextView textView = new TextView(context);
+		textView.setText("おふとんリーディング");
+		textView.setTextSize(25);
+		textView.setGravity(Gravity.CENTER);
+		textView.setTypeface(Typeface.SANS_SERIF);
+		addView(textView);
+		setBackgroundColor(Color.WHITE);
+	}
+
+	@Override
+	protected void onSizeChanged(int w, int h, int oldw, int oldh) {
+		super.onSizeChanged(w, h, oldw, oldh);
+		final int count  = getChildCount();
+		for (int i = 0; i < count; i++) {
+			View view = getChildAt(i);
+			LayoutParams params = view.getLayoutParams();
+			params.width = LayoutParams.MATCH_PARENT;
+			params.height = LayoutParams.MATCH_PARENT;
+			view.setLayoutParams(params);
+		}
+	}
+
+	@Override
+	protected void onLayout(boolean changed, int l, int t, int r, int b) {
+		// TODO Auto-generated method stub
+		final int count = getChildCount();
+		final int left = getLeft();
+		final int top = getTop();
+		final int right = getRight();
+		final int bottom = getBottom();
+		for (int i = 0; i < count; i++) {
+			View view = getChildAt(i);
+			if (view.getVisibility() != View.GONE) {
+				view.layout(left, top, right, bottom);
+			}
+		}
+		invalidate();
+	}
+
+	@Override
+	protected void onMeasure(int widthMeasureSpec, int heightMeasureSpec) {
+		super.onMeasure(widthMeasureSpec, heightMeasureSpec);
+		final int count = getChildCount();
+		for(int i = 0; i < count; i++) {
+			getChildAt(i).measure(widthMeasureSpec, heightMeasureSpec);
+		}
+	}
+}
