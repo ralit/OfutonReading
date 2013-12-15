6d9437c7e7b15a06c09ec7ad77831bd36ea19a59
ralit <ralit@ralitMacBookProMid2012.local>
2013-12-15 22:28:43 +0900

    git addされてなかったらしい

---
+++ src/org/ralit/ofutonreading/OfutonReadingActivity.java
@@ -0,0 +1,11 @@
+package org.ralit.ofutonreading;
+
+import android.app.Activity;
+
+public class OfutonReadingActivity extends Activity {
+
+	public OfutonReadingActivity() {
+		// TODO Auto-generated constructor stub
+	}
+
+}
