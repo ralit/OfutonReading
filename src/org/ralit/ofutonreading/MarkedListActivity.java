7224eca1d65f8724f4d5055e827d7b9e052fb335
ralit <ralit@ralitMacBookProMid2012.local>
2013-12-23 11:18:41 +0900

    MarkedListActivityの設定をごにょごにょ

--- src/org/ralit/ofutonreading/MarkedListActivity.java
+++ src/org/ralit/ofutonreading/MarkedListActivity.java
@@ -2,37 +2,35 @@ package org.ralit.ofutonreading;
 
 import java.io.File;
 import java.io.FileInputStream;
-import java.io.FileNotFoundException;
-import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
 
+import android.os.Bundle;
 import android.app.Activity;
 import android.content.Context;
 import android.content.Intent;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.graphics.Color;
-import android.os.Bundle;
-import android.os.Environment;
-import android.view.GestureDetector;
 import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
-import android.view.ViewGroup.LayoutParams;
 import android.widget.ArrayAdapter;
 import android.widget.ImageView;
 import android.widget.LinearLayout;
 import android.widget.ListView;
+import android.support.v4.app.NavUtils;
 
 public class MarkedListActivity extends Activity {
+
 	private ListView listview;
 	private String bookName;
-
+	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
-		
 		Intent intent = getIntent();
 		if (intent != null) {
 			bookName = intent.getStringExtra("bookName");
@@ -45,7 +43,7 @@ public class MarkedListActivity extends Activity {
 		root.addView(listview);
 		createImageList();
 	}
-
+	
 	void createImageList() {
 		File dir = new File(Fun.DIR + bookName + Fun.MARKER);
 		File[] filelist = dir.listFiles();
@@ -64,7 +62,32 @@ public class MarkedListActivity extends Activity {
 			e.printStackTrace();
 		}
 	}
+
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		getMenuInflater().inflate(R.menu.marked_list, menu);
+		return true;
+	}
+
+	@Override
+	public boolean onOptionsItemSelected(MenuItem item) {
+		switch (item.getItemId()) {
+		case android.R.id.home:
+			// This ID represents the Home or Up button. In the case of this
+			// activity, the Up button is shown. Use NavUtils to allow users
+			// to navigate up one level in the application structure. For
+			// more details, see the Navigation pattern on Android Design:
+			//
+			// http://developer.android.com/design/patterns/navigation.html#up-vs-back
+			//
+			NavUtils.navigateUpFromSameTask(this);
+			return true;
+		}
+		return super.onOptionsItemSelected(item);
+	}
+
 }
+
 
 
 class ImageItem {
